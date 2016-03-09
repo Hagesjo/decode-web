0430153041781b90887ce90ea32c32f460ae3d6a
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2016-03-09 17:49:25 +0100

    Initial functionality for braille

---
+++ main/static/main/js/braille.js
@@ -0,0 +1,4 @@
+function appendBraille(inp) {
+	console.log(inp);
+	$('#braille_decode').val($('#braille_decode').val() + inp);
+}
--- main/views.py
+++ main/views.py
@@ -1,17 +1,25 @@
 # Create your views here.
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from django.conf import settings
+from os import listdir, path
 
 # Create your views here.
 def common(request):
 	return render(request, 'main/common.html')
 
 def braille(request):
-	return render(request, 'main/braille.html')
+	template = loader.get_template('main/braille.html')
+	data = [] # List of tuples: [(image name, id for image used in js)]
+	for image in listdir(path.join(settings.STATIC_ROOT, 'main/img/braille/')):
+		if image.endswith('.gif'):
+			data.append((image, image[:-4]))
+	data.sort()
+	return HttpResponse(template.render({'data' : data}, request))
 
 def caesar(request):
 	return render(request, 'main/caesar.html')
 
 def substi(request):
 	return render(request, 'main/substi.html')
-
